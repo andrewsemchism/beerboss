c7e6c410b392ca3e9ce2ad4398302c2f52041d87
Andrew Semchism <andrew0187@gmail.com>
2020-08-12 19:06:06 -0400

    Add dotenv and gitignore to hide database information

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var mysql = require('mysql');
 var router = express.Router();
+require('dotenv').config()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
   const connection = mysql.createConnection({
-    host : "us-cdbr-east-02.cleardb.com",
-    user : "b295efe6037c4c",
-    password : "8b12d852",
-    database : "heroku_85f2e11d0809ba2"
+    host : process.env.HOST,
+    user : process.env.USER,
+    password : process.env.PASSWORD,
+    database : process.env.DATABASE
   })
 
   connection.query('SELECT * FROM beer', (err, rows, fields) => {

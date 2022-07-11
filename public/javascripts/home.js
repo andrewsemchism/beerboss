ac8d450911342e6d8f3af96233d71e5e7bdcc511
Andrew Semchism <andrew0187@gmail.com>
2022-07-10 22:54:09 -0400

    Style cards on homepage

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,7 +1,18 @@
-var beerAnimation = bodymovin.loadAnimation({
+// Banner beer animation
+const beerAnimation = bodymovin.loadAnimation({
   container: document.getElementById("beerBannerAnimation"),
   renderer: 'svg',
-  loop: true,
+  loop: false,
   autoplay: true,
   path: '../animations/beerBanner.json'
+})
+
+// Start Now button scroll
+$(document).ready(function() {
+  $(".startNowButton").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#using-beerboss").offset().top
+      },
+      'slow');
+  });
 })

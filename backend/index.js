253bdcc8fbea23c55b0e76b0eead4042ef4dbbbb
Andrew Semchism <andrew0187@gmail.com>
2023-08-08 19:04:41 -0400

    Allow docker-compose to use env variables for DB config.

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,10 @@ const mysql = require('mysql2');
 const cors = require('cors');
 
 const db = mysql.createPool({
-  host: 'backend-db', 
-  user: 'user', 
-  password: 'password',
-  database: 'db',
-  port: '3306'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 })
 
 const app = express();

1255d371a95db48f79053a83879f84151701dbd6
Andrew Semchism <andrew0187@gmail.com>
2023-08-08 01:45:25 -0400

    Initial docker setup.

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const db = mysql.createPool({
   host: 'mysql_db', // the host name MYSQL_DATABASE: node_mysql
   user: 'MYSQL_USER', // database user MYSQL_USER: MYSQL_USER
   password: 'MYSQL_PASSWORD', // database user password MYSQL_PASSWORD: MYSQL_PASSWORD
-  database: 'books' // database name MYSQL_HOST_IP: mysql_db
+  database: 'beers' // database name MYSQL_HOST_IP: mysql_db
 })
 
 const app = express();

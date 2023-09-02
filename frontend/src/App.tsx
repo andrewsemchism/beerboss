5143492173efbd119e7b0b83b988c4210a638fcd
Andrew Semchism <andrew0187@gmail.com>
2023-09-02 01:20:02 -0400

    Add best value analyzer page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import CustomNav from './components/CustomNav/CustomNav';
 import All from './components/All/All';
+import Value from './components/Value/Value';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -15,7 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/all" element={<All/>} />
-          <Route path="/value" element={<h1>Value</h1>} />
+          <Route path="/value" element={<Value/>} />
         </Routes>
       </div>
     </BrowserRouter>

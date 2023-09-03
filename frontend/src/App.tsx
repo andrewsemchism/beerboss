4fe9d3574c6923d6c7e3dc3b48f39b200f03467e
Andrew Semchism <andrew0187@gmail.com>
2023-09-03 01:44:55 -0400

    Add about page. FAQ section and Github button.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import CustomNav from './components/CustomNav/CustomNav';
 import All from './components/All/All';
 import Value from './components/Value/Value';
+import About from './components/About/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/all" element={<All/>} />
           <Route path="/value" element={<Value/>} />
+          <Route path="/about" element={<About/>} />
         </Routes>
       </div>
     </BrowserRouter>

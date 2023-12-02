1ad05199b6d68c1c97d0169afbe38454d30b0142
Andrew Semchism <andrew0187@gmail.com>
2023-12-02 00:07:25 -0500

    Format Home.tsx with prettier

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import Banner from '../Banner/Banner';
-import HowTo from '../HowTo/HowTo';
-import footer from './footer.svg';
+import React from "react";
+import Banner from "../Banner/Banner";
+import HowTo from "../HowTo/HowTo";
+import footer from "./footer.svg";
 
 const Home: React.FC = () => {
   return (
     <>
-    <Banner/>
-    <HowTo/>
-    <img
+      <Banner />
+      <HowTo />
+      <img
         style={{
-          marginTop: '50px', 
-          width: '100%',
-          display: 'block',
-          position: 'relative',
-          bottom: '-1px'
+          marginTop: "50px",
+          width: "100%",
+          display: "block",
+          position: "relative",
+          bottom: "-1px",
         }}
         src={footer}
         alt=""
       />
-      <div style={{ height: '50px', backgroundColor: '#3a4047' }}>
-      </div>
+      <div style={{ height: "50px", backgroundColor: "#3a4047" }}></div>
     </>
-    
   );
-}
+};
 
 export default Home;

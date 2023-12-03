cb208cfba9a5785e9b76b196fc311760c1861b94
Andrew Semchism <andrew0187@gmail.com>
2023-12-03 12:24:01 -0500

    Enable ssl connection to server in prod

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,9 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  ssl: {
+    rejectUnauthorized: process.env.NODE_ENV === "production" ? true : false,
+  },
 });
 
 const app = express();

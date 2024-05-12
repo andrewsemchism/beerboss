bdb22e121f1dfabc899eb5a5dc78244ca2a8c917
Andrew Semchism <andrew0187@gmail.com>
2024-05-12 03:14:31 -0400

    Add optional DB port config

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,

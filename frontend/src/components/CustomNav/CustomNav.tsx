4ec4a9d7c93a059d7bf58ef90c0ffbb09403fe22
Andrew Semchism <andrew0187@gmail.com>
2024-05-12 03:05:31 -0400

    Remove Nav alert bar. Scraping script now fixed!

--- frontend/src/components/CustomNav/CustomNav.tsx
+++ frontend/src/components/CustomNav/CustomNav.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Navbar, Nav, Alert } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import './CustomNav.css';
 
@@ -15,43 +15,38 @@ const CustomNav: React.FC = () => {
   };
 
   return (
-    <>
-      <Alert variant="danger" className="nav-alert" style={{ margin: 0, textAlign: 'center' }}>
-        Prices <b>last updated March 19th, 2024</b>. The Beer Store has updated their website and the data scraping script is now broken. A fix is coming soon!
-      </Alert>
-      <Navbar id="beerNav" bg="dark" expand="lg" variant="dark" expanded={expanded}>
-        <Navbar.Brand onClick={() => handleNavLinkClick('')}>BEER BOSS</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarmenu" onClick={() => setExpanded(!expanded)} />
-        <Navbar.Collapse id="navbarmenu">
-          <Nav className="mr-auto">
-            <Nav.Item>
-              <Nav.Link
-                className={activePage === 'all' ? 'active' : ''}
-                onClick={() => handleNavLinkClick('all')}
-              >
-                ALL BEER PRICES
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link
-                className={activePage === 'value' ? 'active' : ''}
-                onClick={() => handleNavLinkClick('value')}
-              >
-                BEST VALUE ANALYZER
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link
-                className={activePage === 'about' ? 'active' : ''}
-                onClick={() => handleNavLinkClick('about')}
-              >
-                ABOUT
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </>
+    <Navbar id="beerNav" bg="dark" expand="lg" variant="dark" expanded={expanded}>
+      <Navbar.Brand onClick={() => handleNavLinkClick('')}>BEER BOSS</Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarmenu" onClick={() => setExpanded(!expanded)} />
+      <Navbar.Collapse id="navbarmenu">
+        <Nav className="mr-auto">
+          <Nav.Item>
+            <Nav.Link
+              className={activePage === 'all' ? 'active' : ''}
+              onClick={() => handleNavLinkClick('all')}
+            >
+              ALL BEER PRICES
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              className={activePage === 'value' ? 'active' : ''}
+              onClick={() => handleNavLinkClick('value')}
+            >
+              BEST VALUE ANALYZER
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              className={activePage === 'about' ? 'active' : ''}
+              onClick={() => handleNavLinkClick('about')}
+            >
+              ABOUT
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
